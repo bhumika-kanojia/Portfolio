4c5ccc64454044036e536f9c13a3ca9513fd2a06
Bhumika Kanojia <bhumika23826@gmail.com>
2026-04-05 13:43:50 +0530

    portfolio commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Dark Mode Toggle
+let themeIcon = document.getElementById('theme-icon');
+
+themeIcon.onclick = () => {
+    document.body.classList.toggle('dark-mode');
+    
+    if(document.body.classList.contains('dark-mode')){
+        themeIcon.classList.remove('bx-moon');
+        themeIcon.classList.add('bx-sun');
+    } else {
+        themeIcon.classList.remove('bx-sun');
+        themeIcon.classList.add('bx-moon');
+    }
+};
+
+// Navbar toggler for mobile
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
